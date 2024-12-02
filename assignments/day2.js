9f837d22e3fd1bfe64e2845e7e065d79f0bdfb6c
Marc Hollander <marchollander@gmail.com>
2024-12-02 20:32:39 +0100

    2.2 solution

--- assignments/day2.js
+++ assignments/day2.js
@@ -3,60 +3,14 @@ const input = await response.text();
 const rows = input.split('\n');
 let safeCount = 0;
 
-for (let i = 0; i < rows.length; i++) {
-    const row = rows[i];
-
-    let safe = false;
-    let failed = false;
-    let prevNumber;
-    let dir;
-
-    row.split(' ').forEach((item) => {
-        if (failed) {
-            return;
-        }
-
-        const curNumber = parseInt(item);
-        if (prevNumber) {
-            if (!dir) {
-                dir = (curNumber > prevNumber) ? 'up' : 'down';
-            }
-
-            if (dir === 'up') {
-                const numberTest = prevNumber + 4
-                safe = (curNumber > prevNumber && curNumber < numberTest)
-                if (!safe) {
-                    failed = true;
-                }
-            } else if (dir === 'down') {
-                const numberTest = prevNumber - 4
-                safe = (curNumber < prevNumber && curNumber > numberTest)
-                if (!safe) {
-                    failed = true;
-                }
-            }
-        }
-
-        prevNumber = curNumber;
-    });
-
-    if (safe) {
-        safeCount++;
-    }
-}
-
-document.getElementById('2.1').innerHTML = safeCount;
-
-let safeCountPartTwo = 0;
-
-function analyzeNumbersForPartTwo(numbers, safe, failures) {
+function analyzeNumbers(numbers, allowedFailures = 0, safe = false, failures = 0) {
     let prevNumber;
     let dir;
 
     numbers.forEach((item) => {
         let failed = false;
 
-        if (failures > 1) {
+        if (failures > allowedFailures) {
             return;
         }
 
@@ -98,16 +52,39 @@ for (let i = 0; i < rows.length; i++) {
     let failures = 0;
 
     const numbers = row.split(' ');
-    [safe, failures] = analyzeNumbersForPartTwo(numbers, safe, failures);
+    [safe, failures] = analyzeNumbers(numbers);
+
+    if (safe) {
+        safeCount++;
+    }
+}
+
+document.getElementById('2.1').innerHTML = safeCount;
+
+let safeCountPartTwo = 0;
+
+for (let i = 0; i < rows.length; i++) {
+    const row = rows[i];
+
+    let safe = false;
+    let failures = 0;
+
+    const numbers = row.split(' ');
+    [safe, failures] = analyzeNumbers(numbers, 1);
 
     if (failures > 1) {
-        failures = 1;
-        numbers.shift();
+        // If a row has failed, then try to remove each number one by one, to see if the row
+        // validates without that one number.
+        for (let x = 0; x < numbers.length; x++) {
+            failures = 1;
+            const newNumbers = row.split(' ');
+            newNumbers.splice(x, 1);
 
-        [safe, failures] = analyzeNumbersForPartTwo(numbers, safe, failures);
+            [safe, failures] = analyzeNumbers(newNumbers , 1, safe, failures);
 
-        if (failures > 1) {
-            console.log(i, {row, safe, failures});
+            if (safe) {
+                break;
+            }
         }
     }
 
@@ -115,7 +92,5 @@ for (let i = 0; i < rows.length; i++) {
         safeCountPartTwo++;
     }
 }
-
-// 657 wrong
 
 document.getElementById('2.2').innerHTML = safeCountPartTwo;
