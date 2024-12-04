80c53b4c18b63c9cf257884297f15851e6b1921a
Marc Hollander <marchollander@gmail.com>
2024-12-04 07:17:16 +0100

    3.2 solution

--- assignments/day3.js
+++ assignments/day3.js
@@ -4,10 +4,29 @@ const input = await response.text();
 const regex = /mul\([(0-9]{1,3},[0-9]{1,3}\)/g;
 const records = input.match(regex);
 
-let totalPartOne = 0;
-for (let i = 0; i < records.length; i++) {
-    const mul = records[i].replace(/mul\(([(0-9]{1,3},[0-9]{1,3})\)/, '$1').split(',');
-    totalPartOne += parseInt(mul[0]) * parseInt(mul[1]);
+function parseMultipliers(records) {
+    let total = 0;
+
+    for (let i = 0; i < records.length; i++) {
+        const mul = records[i].replace(/mul\(([(0-9]{1,3},[0-9]{1,3})\)/, '$1').split(',');
+        total += parseInt(mul[0]) * parseInt(mul[1]);
+    }
+
+    return total;
 }
 
+let totalPartOne = parseMultipliers(records);
+
 document.getElementById('3.1').innerText = totalPartOne;
+
+const regexPartTwo = /do\(\)(.*?)don't\(\)|do\(\)(.*)/g;
+const recordspartTwo = input.match(regexPartTwo);
+
+let totalPartTwo = 0;
+
+for (let i = 0; i < recordspartTwo.length; i++) {
+    const records = recordspartTwo[i].match(regex);
+    totalPartTwo += parseMultipliers(records);
+}
+
+document.getElementById('3.2').innerText = totalPartTwo;
