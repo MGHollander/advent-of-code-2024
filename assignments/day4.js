866afdaa07fd751a590027197cf639d7b507c5b8
Marc Hollander <marchollander@gmail.com>
2024-12-04 16:24:17 +0100

    day 4 dry the code a little

--- assignments/day4.js
+++ assignments/day4.js
@@ -7,8 +7,8 @@ const chars = rows[0].split('');
 let xmasCount = 0;
 let masCount = 0;
 
-function isXmas(char1, char2, char3, char4) {
-    return char1 === 'X' && char2 === 'M' && char3 === 'A' && char4 === 'S';
+function isXmas(char1, char2, char3) {
+    return char1 === 'M' && char2 === 'A' && char3 === 'S';
 }
 
 function isMas(char1, char2) {
@@ -19,37 +19,45 @@ for (let x = 0; x < rows.length; x++) {
     for (let y = 0; y < chars.length; y++) {
         const currentChar = rows[x][y];
         if (currentChar === 'X') {
-            // Horizontal left to right
-            if (rows[x] && isXmas(currentChar, rows[x][y + 1], rows[x][y + 2], rows[x][y + 3])) {
-                xmasCount++;
+            if (rows[x]) {
+                // Horizontal left to right
+                if (isXmas(rows[x][y + 1], rows[x][y + 2], rows[x][y + 3])) {
+                    xmasCount++;
+                }
+                // Horizontal right to left
+                if (isXmas(rows[x][y - 1], rows[x][y - 2], rows[x][y - 3])) {
+                    xmasCount++;
+                }
             }
-            // Horizontal right to left
-            if (rows[x] && isXmas(currentChar, rows[x][y - 1], rows[x][y - 2], rows[x][y - 3])) {
-                xmasCount++;
+
+            if (rows[x + 1] && rows[x + 2] && rows[x + 3]) {
+                // Vertical top to bottom
+                if (isXmas(rows[x + 1][y], rows[x + 2][y], rows[x + 3][y])) {
+                    xmasCount++;
+                }
+                // Vertical top left to bottom right
+                if (isXmas(rows[x + 1][y + 1], rows[x + 2][y + 2], rows[x + 3][y + 3])) {
+                    xmasCount++;
+                }
+                // Vertical top right to bottom left
+                if (isXmas(rows[x + 1][y - 1], rows[x + 2][y - 2], rows[x + 3][y - 3])) {
+                    xmasCount++;
+                }
             }
-            // Vertical top to bottom
-            if (rows[x + 1] && rows[x + 2] && rows[x + 3] && isXmas(currentChar, rows[x + 1][y], rows[x + 2][y], rows[x + 3][y])) {
-                xmasCount++;
-            }
-            // Vertical bottom to top
-            if (rows[x - 1] && rows[x - 2] && rows[x - 3] && isXmas(currentChar, rows[x - 1][y], rows[x - 2][y], rows[x - 3][y])) {
-                xmasCount++;
-            }
-            // Vertical top left to bottom right
-            if (rows[x + 1] && rows[x + 2] && rows[x + 3] && isXmas(currentChar, rows[x + 1][y + 1], rows[x + 2][y + 2], rows[x + 3][y + 3])) {
-                xmasCount++;
-            }
-            // Vertical top right to bottom left
-            if (rows[x + 1] && rows[x + 2] && rows[x + 3] && isXmas(currentChar, rows[x + 1][y - 1], rows[x + 2][y - 2], rows[x + 3][y - 3])) {
-                xmasCount++;
-            }
-            // Vertical bottom right to top left
-            if (rows[x - 1] && rows[x - 2] && rows[x - 3] && isXmas(currentChar, rows[x - 1][y - 1], rows[x - 2][y - 2], rows[x - 3][y - 3])) {
-                xmasCount++;
-            }
-            // Vertical bottom left to top right
-            if (rows[x - 1] && rows[x - 2] && rows[x - 3] && isXmas(currentChar, rows[x - 1][y + 1], rows[x - 2][y + 2], rows[x - 3][y + 3])) {
-                xmasCount++;
+
+            if (rows[x - 1] && rows[x - 2] && rows[x - 3]) {
+                // Vertical bottom to top
+                if (isXmas(rows[x - 1][y], rows[x - 2][y], rows[x - 3][y])) {
+                    xmasCount++;
+                }
+                // Vertical bottom right to top left
+                if (isXmas(rows[x - 1][y - 1], rows[x - 2][y - 2], rows[x - 3][y - 3])) {
+                    xmasCount++;
+                }
+                // Vertical bottom left to top right
+                if (isXmas(rows[x - 1][y + 1], rows[x - 2][y + 2], rows[x - 3][y + 3])) {
+                    xmasCount++;
+                }
             }
         }
 
