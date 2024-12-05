1d3d46420ec2b9a8da2b1186827bc65def80697a
Marc Hollander <marchollander@gmail.com>
2024-12-05 14:16:25 +0100

    Make the UI dynamic

---
+++ bootstrap.js
@@ -0,0 +1,34 @@
+const table = document.getElementById('table');
+
+for (let i = 1; i < 26; i++) {
+    const tr = document.createElement('tr');
+
+    const th = document.createElement('th');
+    th.innerHTML = `Day ${i}`;
+    tr.appendChild(th);
+
+    const tdOne = document.createElement('td');
+    tdOne.setAttribute('id', `${i}.1`)
+    tr.appendChild(tdOne);
+
+    const tdTwo = document.createElement('td');
+    tdTwo.setAttribute('id', `${i}.2`)
+    tr.appendChild(tdTwo);
+
+    // Check the day of the month. If it is higher than the current day, the puzzle is not available yet.
+    const tdThree = document.createElement('td');
+    tdThree.innerHTML = (i > new Date().getDate()) ? `N/A` : `<a href="https://adventofcode.com/2024/day/${i}">puzzle</a>`;
+    tr.appendChild(tdThree);
+
+    table.appendChild(tr);
+
+}
+
+for (let i = 1; i < 26; i++) {
+    const filePath = `./assignments/day${i}.js`;
+    const response = await fetch(filePath);
+    if (response.ok) {
+        break;
+    }
+    import(filePath);
+}
